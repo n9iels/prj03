381671609921cb0cf30197f1249e55dcb4ab5cde
Daniel <danielmolenaar@hotmail.nl>
2016-04-06 20:57:05 +0200

    Added basic Twitter streaming

---
+++ Twitter Server/TwitterApi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TwitterApi;
+
+namespace TwitterApi {
+
+    internal class Program {
+
+        private static void Main(string[] args) {
+
+
+            TwitterStream stream = new TwitterStream();
+            stream.Start();
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ Twitter Server/TwitterApi/TwitterStream.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Tweetinvi;
+using Tweetinvi.Core.Events.EventArguments;
+using Tweetinvi.Core.Interfaces.Models;
+using Tweetinvi.Core.Parameters;
+using Tweetinvi.Streams;
+
+namespace TwitterApi {
+
+    public class TwitterStream {
+        
+        public FilteredStream Stream { get; }
+
+        public event EventHandler<MatchedTweetReceivedEventArgs> TweetReceived {
+            add { Stream.TweetReceived += value; }
+            remove { Stream.TweetReceived -= value; }
+        }
+
+        public TwitterStream() {
+            // Authentication
+            Auth.SetUserCredentials(Properties.Settings.Default.OauthConsumerKey,
+                Properties.Settings.Default.OauthConsumerSecret, Properties.Settings.Default.OauthToken,
+                Properties.Settings.Default.OauthTokenSecret);
+
+            // Creating the stream
+            Stream = (FilteredStream) Tweetinvi.Stream.CreateFilteredStream();
+        }
+
+        public void Start() {
+            // Start listening to TweetReceived event using the Received method.
+            Stream.MatchingTweetReceived += Received;
+
+            // GeoCoordinates Rotterdam
+            ICoordinates sw = new Coordinates(4.400054, 51.870962);
+            ICoordinates ne = new Coordinates(4.566633, 51.991821);
+            Stream.AddLocation(sw, ne);
+
+            Stream.StartStreamMatchingAllConditions();
+
+        }
+
+        private void Received(object sender, MatchedTweetReceivedEventArgs e) {
+
+            // Main information needed for database
+            long id = e.Tweet.Id;
+            string text = e.Tweet.Text;
+            DateTime created = e.Tweet.CreatedAt;
+            ICoordinates coords = e.Tweet.Coordinates;
+            IPlace place = e.Tweet.Place;
+
+            // Information for potential future us.
+
+            int retweets = e.Tweet.RetweetCount;
+            int favouriteCount = e.Tweet.FavoriteCount;
+
+        }
+    }
+}
