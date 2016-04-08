d1cfd76d88aab11fb61f0b411dd1055054c67733
Niels van der Veer (n9iels) <n.i.vdveer@gmail.com>
2016-04-08 15:46:50 +0200

    Refactor code

--- Buienradar Server/Buienradar Server/Program.cs
+++ Buienradar Server/Buienradar Server/Program.cs
@@ -1,86 +1,35 @@
 ﻿using System;
-using System.Configuration;
-using MySql.Data.MySqlClient;
-using System.Xml.Linq;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Buienradar_Server
 {
     class Program
     {
-        public static MySqlConnection conn;
-
         static void Main(string[] args)
         {
-            string LastDate = "";
-
             while (true)
             {
-                Dictionary<string, string> element = GetWeatherStation(6344);
+                // Get whole document
+                try {
+                    Dictionary<string, string> data = XmlFilter.FilterData(WeatherRetrieval.GetData("http://xml.buienradar.nl"), 6344);
+                    DatabaseParser.UploadToDatabase(data);
 
-                if (LastDate != element["datum"])
+                    // Console log
+                    Console.WriteLine("Weather updated " + data["datum"]);
+                }
+                catch (WebException)
                 {
-                    // Do something wit the database
-                    InsertIntoDatabase(element);
-
-                    // Set last date
-                    LastDate = element["datum"];
+                    Console.WriteLine("Unable to load XML Data");
                 }
 
-                System.Threading.Thread.Sleep(300000);
+                // Wait 5 minutes
+                System.Threading.Thread.Sleep(5 * 1000 * 60);
             }
         }
-
-        public static Dictionary<string, string> GetWeatherStation (int id)
-        {
-            XDocument doc = XDocument.Load("http://xml.buienradar.nl");
-            XElement filteredElements = doc.Descendants("weerstation").Where(d => (int)d.Attribute("id") == id).Select(d => d).Single();
-
-            return CreateList(filteredElements);
-        }
-
-        public static Dictionary<string, string> CreateList(XElement element)
-        {
-            Dictionary<string, string> data = new Dictionary<string, string>();
-
-            data.Add("stationnaam", element.Element("stationnaam").Value);
-            data.Add("datum", element.Element("datum").Value);
-            data.Add("temperatuurGC", element.Element("temperatuurGC").Value);
-            data.Add("windsnelheidMS", element.Element("windsnelheidMS").Value);
-            data.Add("windsnelheidBF", element.Element("windsnelheidBF").Value);
-            data.Add("luchtvochtigheid", element.Element("luchtvochtigheid").Value);
-            data.Add("regenMMPU", element.Element("regenMMPU").Value);
-            data.Add("id", element.Element("icoonactueel").Attribute("ID").Value);
-            data.Add("type", element.Element("icoonactueel").Attribute("zin").Value);
-
-            return data;
-        }
-
-        
-        public static void InsertIntoDatabase(Dictionary<string, string> data)
-        {
-            // Connect to the database
-            string connectionString = ConfigurationManager.ConnectionStrings["dataBeest"].ConnectionString;
-            conn = new MySqlConnection(connectionString);
-            conn.Open();
-
-            // Create SQL command
-            MySqlCommand command = conn.CreateCommand();
-            command.CommandText  = "INSERT IGNORE INTO weather_condition VALUES(@date, @temperaturegc, @windspeedms, @windspeedbf, @humidity, @rainmmpu);";
-            command.CommandText += "INSERT IGNORE INTO weather_types VALUES(@id, @name);";
-            command.Parameters.AddWithValue("@date", DateTime.Parse(data["datum"]));
-            command.Parameters.AddWithValue("@temperaturegc", data["temperatuurGC"]);
-            command.Parameters.AddWithValue("@windspeedms", data["windsnelheidMS"]);
-            command.Parameters.AddWithValue("@windspeedbf", data["windsnelheidBF"]);
-            command.Parameters.AddWithValue("@humidity", data["luchtvochtigheid"]);
-            command.Parameters.AddWithValue("@rainmmpu", data["regenMMPU"]);
-            command.Parameters.AddWithValue("@id", data["id"]);
-            command.Parameters.AddWithValue("@name", data["stationnaam"]);
-            command.ExecuteNonQuery();
-
-            conn.Close();
-        }
-        
     }
 }
---
+++ Buienradar Server/Buienradar Server/XmlFilter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Buienradar_Server
+{
+    class XmlFilter
+    {
+        public static Dictionary<string, string> FilterData(XDocument doc, int stationCode)
+        {
+            XElement filteredElements = doc.Descendants("weerstation").Where(d => (int)d.Attribute("id") == stationCode).Select(d => d).Single();
+
+            return CreateList(filteredElements);
+        }
+
+        private static Dictionary<string, string> CreateList(XElement element)
+        {
+            Dictionary<string, string> data = new Dictionary<string, string>();
+
+            data.Add("datum", element.Element("datum").Value);
+            data.Add("temperatuurGC", element.Element("temperatuurGC").Value);
+            data.Add("windsnelheidMS", element.Element("windsnelheidMS").Value);
+            data.Add("windsnelheidBF", element.Element("windsnelheidBF").Value);
+            data.Add("luchtvochtigheid", element.Element("luchtvochtigheid").Value);
+            data.Add("regenMMPU", element.Element("regenMMPU").Value);
+            data.Add("id", element.Element("icoonactueel").Attribute("ID").Value);
+            data.Add("type", element.Element("icoonactueel").Attribute("zin").Value);
+
+            return data;
+        }
+    }
+}
