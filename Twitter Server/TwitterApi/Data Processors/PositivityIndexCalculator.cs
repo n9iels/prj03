572cf848d79e3c57a60badf76e22e3cfc52d9d3b
Daniel <danielmolenaar@aol.nl>
2016-04-14 12:17:30 +0200

    Stabilized servers

--- Twitter Server/TwitterApi/Data Processors/PositivityIndexCalculator.cs
+++ Twitter Server/TwitterApi/Data Processors/PositivityIndexCalculator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using MySql.Data.MySqlClient;
 using Tweetinvi.Core.Interfaces;
@@ -25,8 +26,8 @@ namespace TwitterApi.Data_Processors {
         }
 
         protected double Calculate(string tweetText) {
-            Dictionary<string, int> positiveWords = _matchFinder.FindMatches(tweetText, "Word Lists/Positive-Words-NL.txt", "Word Lists/Positive-Words-EN.txt");
-            Dictionary<string, int> negativeWords = _matchFinder.FindMatches(tweetText, "Word Lists/Negative-Words-NL.txt" , "Word Lists/Negative-Words-EN.txt");
+            Dictionary<string, int> positiveWords = _matchFinder.FindMatches(tweetText, "Resources/Word Lists/Positive-Words-NL.txt", "Resources/Word Lists/Positive-Words-EN.txt");
+            Dictionary<string, int> negativeWords = _matchFinder.FindMatches(tweetText, "Resources/Word Lists/Negative-Words-NL.txt" , "Resources/Word Lists/Negative-Words-EN.txt");
 
             double positive = positiveWords.Sum(x => Math.Pow(x.Value, 0.5));
             double negative = negativeWords.Sum(x => Math.Pow(x.Value, 0.5));
@@ -34,29 +35,46 @@ namespace TwitterApi.Data_Processors {
         }
 
         protected void UploadToDatabase(ITweet tweet, double pIndex) {
-            using (
-                MySqlConnection conn =
-                    new MySqlConnection(ConfigurationManager.ConnectionStrings["dataBeest"].ConnectionString)) {
-                conn.Open();
-                MySqlCommand com = MySqlQueryGenerator.GenerateQuery(conn,
-                    "INSERT INTO twitter_tweets VALUES(@id,@created,@profileId,@placeId,@text,@coordLong,@coordLat,@pIndex,@language);",
-                    tweet.Id, tweet.CreatedAt, tweet.CreatedBy.Id, tweet.Place?.IdStr, tweet.Text,
-                    tweet.Coordinates?.Longitude, tweet.Coordinates?.Latitude, pIndex, tweet.Language.ToString());
-                com.ExecuteNonQuery();
+            try {
+                using (
+                    MySqlConnection conn =
+                        new MySqlConnection(ConfigurationManager.ConnectionStrings["dataBeest"].ConnectionString)) {
+                    conn.Open();
+                    MySqlCommand com = MySqlQueryGenerator.GenerateQuery(conn,
+                        "INSERT INTO twitter_tweets VALUES(@id,@created,@profileId,@placeId,@text,@coordLong,@coordLat,@pIndex,@language);",
+                        tweet.Id, tweet.CreatedAt, tweet.CreatedBy.Id, tweet.Place?.IdStr, tweet.Text,
+                        tweet.Coordinates?.Longitude, tweet.Coordinates?.Latitude, pIndex, tweet.Language.ToString());
+                    com.ExecuteNonQuery();
 
-                if (tweet.Place != null) {
-                    MySqlCommand comPlaces = MySqlQueryGenerator.GenerateQuery(conn,
-                        "INSERT IGNORE INTO twitter_places VALUES(@id,@name,@fullName)", tweet.Place.IdStr, tweet.Place.Name,
-                        tweet.Place.FullName);
-                    comPlaces.ExecuteNonQuery();
+                    if (tweet.Place != null) {
+                        MySqlCommand comPlaces = MySqlQueryGenerator.GenerateQuery(conn,
+                            "INSERT IGNORE INTO twitter_places VALUES(@id,@name,@fullName)", tweet.Place.IdStr,
+                            tweet.Place.Name, tweet.Place.FullName);
+                        comPlaces.ExecuteNonQuery();
+                    }
+
+                    MySqlCommand comProfiles = MySqlQueryGenerator.GenerateQuery(conn,
+                        "INSERT IGNORE INTO twitter_profiles VALUES (@id,@name,@screenName,@profileImage);",
+                        tweet.CreatedBy.Id, tweet.CreatedBy.Name, tweet.CreatedBy.ScreenName,
+                        tweet.CreatedBy.ProfileImageUrl);
+                    comProfiles.ExecuteNonQuery();
                 }
-
-                MySqlCommand comProfiles = MySqlQueryGenerator.GenerateQuery(conn,
-                    "INSERT IGNORE INTO twitter_profiles VALUES (@id,@name,@screenName,@profileImage);", tweet.CreatedBy.Id,
-                    tweet.CreatedBy.Name, tweet.CreatedBy.ScreenName, tweet.CreatedBy.ProfileImageUrl);
-                comProfiles.ExecuteNonQuery();
+                _logger.Log(new TweetLogData(tweet, pIndex));
             }
-            _logger.Log(new TweetLogData(tweet, pIndex));
+            catch (MySqlException ex) {
+                string[] lines = {
+                    "=================",
+                    $"Error at time (DateTime.Now) : {DateTime.Now}",
+                    $"Error at message : {tweet.Text}",
+                    $"Error log : {ex.Message}",
+                    "",
+                    ""
+                };
+                try {
+                    File.AppendAllLines("error.log", lines);
+                }
+                catch { }
+            }
         }
     }
 }
